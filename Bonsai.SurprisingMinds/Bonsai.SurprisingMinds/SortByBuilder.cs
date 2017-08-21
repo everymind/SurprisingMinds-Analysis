aadfff86b47badc568683d39e020a902e7352b7c
glopesdev <glopesdev@users.noreply.github.com>
2017-08-21 15:03:52 +0100

    Renamed surprising minds namespace

--- Bonsai.SurprisingMinds/Bonsai.SurprisingMinds/SortByBuilder.cs
+++ Bonsai.SurprisingMinds/Bonsai.SurprisingMinds/SortByBuilder.cs
@@ -9,7 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
 
-namespace SurprisingMinds
+namespace Bonsai.SurprisingMinds
 {
     /// <summary>
     /// Represents a combinator that sorts the elements of the input enumerable
