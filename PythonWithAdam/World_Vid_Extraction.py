63f6b6703cf007dbac75e9a7b74689f69ba645bf
taunsquared <danbee@alum.mit.edu>
2019-07-19 12:25:07 +0100

    for easier human reading

--- PythonWithAdam/World_Vid_Extraction.py
+++ PythonWithAdam/World_Vid_Extraction.py
@@ -540,6 +540,9 @@ stim_name_to_float = {"stimuli024": 24.0, "stimuli025": 25.0, "stimuli026": 26.0
 stim_float_to_name = {24.0: "stimuli024", 25.0: "stimuli025", 26.0: "stimuli026", 27.0: "stimuli027", 28.0: "stimuli028", 29.0: "stimuli029"}
 ### DAYS THAT CANNOT BE UNZIPPED ###
 invalid_zipped = ['2017-12-28','2018-01-25']
+### -------------------------------------------- ###
+### -------------------------------------------- ###
+### -------------------------------------------- ###
 # BEGIN WORLD VID FRAME EXTRACTION/AVERAGING #
 for item in zipped_data:
     this_day_date = item[:-4].split('_')[1]
