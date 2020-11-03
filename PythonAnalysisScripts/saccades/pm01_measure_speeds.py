16d65c806bc51a70801709cb4a24bd15f5137c3f
taunsquared <danbee@alum.mit.edu>
2020-11-03 18:07:48 +0000

    updated pseudo script

--- PythonAnalysisScripts/saccades/pm01_measure_speeds.py
+++ PythonAnalysisScripts/saccades/pm01_measure_speeds.py
@@ -5,25 +5,21 @@ import matplotlib.pyplot as plt
 import datetime
 import os.path
 
-# Measures pupil speeds in each trial and saves intermediate binary file
-
-# Grab today's date
+# grab today's date
 now = datetime.datetime.now()
 todays_datetime = datetime.datetime.today().strftime('%Y%m%d-%H%M%S')
 current_working_directory = os.getcwd()
 
 # Specify relevant data/output folders - laptop
 data_folder = r'C:\Users\taunsquared\Dropbox\SurprisingMinds\analysis\dataPythonWorkflows'
-folder_name = 'SurprisingMinds*'
-# Specify relevant data/output folders - office (windows)
+plots_folder = r'C:\Users\taunsquared\Dropbox\SurprisingMinds\analysis\plots\saccade_detector'
+intermediate_data_folder = r'C:\Users\taunsquared\Dropbox\SurprisingMinds\analysis\intermediates'
+# Specify relevant data/output folders - office
 #data_folder = r'C:\Users\Kampff_Lab\Dropbox\SurprisingMinds\analysis\dataPythonWorkflows'
-#folder_name = 'SurprisingMinds*'
-# Specify relevant data/output folders - office (linux)
-#data_folder = r'/home/kampff/Data/Surprising'
-#folder_name = 'surprisingminds*'
+#plots_folder = r'C:\Users\Kampff_Lab\Dropbox\SurprisingMinds\analysis\plots\saccade_detector'
 
 # Find daily folders
-daily_folders = glob.glob(data_folder + os.sep + folder_name)
+daily_folders = glob.glob(data_folder + os.sep + 'SurprisingMinds*')
 # If you only want to find saccades in a subset of the data...
 #daily_folders = daily_folders[10:100]
 
@@ -39,7 +35,7 @@ for df_C, daily_folder_count in enumerate(daily_folders):
 print('Number of files: {n}'.format(n=num_files))
 
 # Create an empty folder for speed data [CAUTION, DELETES ALL PREVIOUS SPEED FILES]
-speed_data_folder = data_folder + os.sep + 'speeds'
+speed_data_folder = intermediate_data_folder + os.sep + 'speeds'
 if not os.path.exists(speed_data_folder):
     #print("Creating plots folder.")
     os.makedirs(speed_data_folder)
@@ -51,7 +47,6 @@ if os.path.exists(speed_data_folder):
 
 # Extract pupil tracking data and generate "speed" per frame for each eye video
 trial_count = 0
-bad_trials = 0
 stim_count = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0}
 for df, daily_folder in enumerate(daily_folders):
 
@@ -60,7 +55,6 @@ for df, daily_folder in enumerate(daily_folders):
     if(len(csv_paths) == 0):
         csv_paths = glob.glob(daily_folder + os.sep + 'Analysis' + os.sep + 'csv'+ os.sep + '*.csv')
     num_files = len(csv_paths)
-    print("Total Number of CSV files: " + str(num_files))
 
     # Process all csv files in a folder
     for cp, csv_path in enumerate(csv_paths):
@@ -87,8 +81,7 @@ for df, daily_folder in enumerate(daily_folders):
 
         # Exclude crappy trials
         if(len(good_indices) < 200):
-            bad_trials += 1
-            continue
+            break
         good_x = x[good_indices]
         good_y = y[good_indices]
         good_area = area[good_indices]
@@ -131,7 +124,7 @@ for df, daily_folder in enumerate(daily_folders):
         speed = np.float32(speed)
 
         # Store
-        output_path = speed_data_folder + os.sep + 'stim%d_%s_speed_%d.data' % (stimulus, eye, trial_count)
+        output_path = speed_data_folder + os.sep + 'stim%d_%s_peak_%d.data' % (stimulus, eye, trial_count)
         speed.tofile(output_path)
         trial_count = trial_count + 1
 
@@ -153,6 +146,4 @@ for df, daily_folder in enumerate(daily_folders):
             plt.show()
 
         # Report progress
-        print("Trial Count: " + str(trial_count) + " | " + str(bad_trials))
-
-#FIN
+        print(trial_count)
